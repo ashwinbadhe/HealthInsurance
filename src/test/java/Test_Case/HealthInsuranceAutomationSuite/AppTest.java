af4115af9f59f2e6a78a676eabee7d23d24735b0
ashwinbadhe <badheashwin354@gmail.com>
2026-05-01 18:22:39 +0530

    1st commit

---
+++ src/test/java/Test_Case/HealthInsuranceAutomationSuite/AppTest.java
@@ -0,0 +1,19 @@
+package Test_Case.HealthInsuranceAutomationSuite;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
